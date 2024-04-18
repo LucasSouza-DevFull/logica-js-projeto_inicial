5b25417a2bf3c956d7cdf6eab815ff389998a7b7
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-18 17:38:42 -0300

    else and alert

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ alert ('Boas vindas ao jogo do número secreto')
 let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30')
 
+// se o chute for igual ao numero secreto, executa uma funçao
 if (chute == numeroSecreto) {
-    console.log('Isso ai, Você descobriu o número secreto (29)');
+    alert('Isso ai, Você descobriu o número secreto (29)');
+} else {
+    alert('Você errou :(')
 }
