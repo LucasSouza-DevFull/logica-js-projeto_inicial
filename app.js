9cd3e3726224ef311b48bb130bce6c0ab7f63367
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-18 17:41:49 -0300

    console numero secreto

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 alert ('Boas vindas ao jogo do número secreto')
 let numeroSecreto = 29;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30')
 
 // se o chute for igual ao numero secreto, executa uma funçao
