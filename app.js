b1c8eed9f2dcfb384ca7a2a139637d0a42c4be40
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-23 17:50:53 -0300

    ajustes corpo code

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ while(chute != numeroSecreto){
     if (chute == numeroSecreto) {
          alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto}`);
     } else {
-    if (chute > numeroSecreto){
+      if (chute > numeroSecreto){
              alert(`O número secreto é menor que ${chute}`);
-    } else {
+      } else {
              alert(`O numero secreto é maior que ${chute} `);
-    }
+      }
     }
 }
