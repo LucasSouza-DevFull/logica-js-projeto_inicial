5694a6e1386ae9f5c54e32f2bd72e51e919fe9bd
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-23 17:38:16 -0300

    novas condiçoes

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ let chute = prompt('Escolha um número entre 1 e 30')
 if (chute == numeroSecreto) {
     alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if (chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O numero secreto é maior que ${chute} `);
+    }
 }
