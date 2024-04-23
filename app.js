786aa57d2284a42b223c7bfae19d58eb91ee51f3
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-23 17:41:03 -0300

    comentario

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ let numeroSecreto = 29;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30')
 
-//enquanto
-while
+//enquanto o chute não for igual ao numero secreto, continue repetindo
+while(){
+
+}
 // se o chute for igual ao numero secreto, executa uma funçao
 if (chute == numeroSecreto) {
     alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto}`);
