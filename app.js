d9ea0ad6d0ebc7e730244458694f20283536ef91
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-23 17:40:18 -0300

    while com comentario o que faz

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let numeroSecreto = 29;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30')
 
+//enquanto
+while
 // se o chute for igual ao numero secreto, executa uma funçao
 if (chute == numeroSecreto) {
     alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto}`);
