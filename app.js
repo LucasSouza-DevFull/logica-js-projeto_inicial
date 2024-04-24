b7d4782dcbde91a7e1cbd707bb1f94e6a4e6ceb7
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-04-24 17:45:18 -0300

    teste tentativa

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 30');
     // se o chute for igual ao numero secreto, executa uma funçao
     if (chute == numeroSecreto) {
-         alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto}`);
+         alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
       if (chute > numeroSecreto){
              alert(`O número secreto é menor que ${chute}`);
