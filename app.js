4d0251785bc2cbc54d514b6d2153935941f0011e
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-08-14 17:32:56 -0300

    ajuste de frase no singular com IF

--- app.js
+++ app.js
@@ -19,5 +19,8 @@ while(chute != numeroSecreto){
       tentativas++;
     }
 }
-
-alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+if (tentativas > 1){
+       alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+       alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
