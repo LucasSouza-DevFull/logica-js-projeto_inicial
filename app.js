b6fd06b405b0b1482ee37875bbd2e5497d505356
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-12-04 17:42:28 -0300

    linha 2 let acrescentado variavel numeroMaximo com Math.random

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert ('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = parseInt(Math.random() * 100 + 1);
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto o chute não for igual ao numero secreto, continue repetindo
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se o chute for igual ao numero secreto, executa uma funçao
     if (chute == numeroSecreto) {
          break;
