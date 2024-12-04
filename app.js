aa039a8d41f21eeb8c0b25569e3ed19f0cf45c35
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-12-04 17:25:46 -0300

    espaço

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ while(chute != numeroSecreto){
 
  let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
  alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
-
+ 
 // if (tentativas > 1){
 //        alert(`Isso ai, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
 // } else {
